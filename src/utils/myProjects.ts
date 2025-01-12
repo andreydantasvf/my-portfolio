5be53d9e043831212116a6a8ee0c8db1344ac43b
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-12 20:24:03 -0300

    feat: import my projects github

---
+++ src/utils/myProjects.ts
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    title: 'Jokenpo',
+    imgURL:
+      'https://res.cloudinary.com/andreydantas/image/upload/v1642863558/Jokenpo-App/desktop_xkddvq.png',
+    details:
+      'Teste suas hábilidades no famoso Jokenpo contra uma máquina. Construído com javascript puro, um valor aleatório é gerado e decidirá se você ganhou ou perdeu a disputa de pedra, papel ou tesoura.',
+    githubLink: 'https://github.com/andreydantasvf/Jokenpo',
+    webLink: 'https://andreydantasvf.github.io/Jokenpo/'
+  },
+  {
+    title: 'Wizzi Flight',
+    imgURL:
+      'https://cdn.discordapp.com/attachments/282658921712320513/1153780016606351501/image.png',
+    details: 'Página de formulário para registro de passagens aéreas.',
+    githubLink: 'https://github.com/andreydantasvf/Wizzi-Teste',
+    webLink: 'https://wizzi-teste.vercel.app/'
+  },
+  {
+    title: 'NFT MarketPlace',
+    imgURL:
+      'https://res.cloudinary.com/andreydantas/image/upload/v1675122213/NFT-MarketPlace_oexdms.png',
+    details:
+      'Landing Page construída apenas em Next js e Tailwind Css para simular uma página de vendas de NFTs.',
+    githubLink: 'https://github.com/andreydantasvf/NFT-Marketplace-2.0',
+    webLink: 'https://nftmarketplace-hazel.vercel.app/'
+  },
+  {
+    title: 'Deliverizé',
+    imgURL:
+      'https://cdn.discordapp.com/attachments/282658921712320513/1153780751356153956/image.png',
+    details:
+      'Página simulando o momento da compra de um lanche em um app fictício.',
+    githubLink: 'https://github.com/andreydantasvf/Deliverize',
+    webLink: 'https://deliverize.netlify.app/'
+  },
+  {
+    title: 'YourFood',
+    imgURL:
+      'https://raw.githubusercontent.com/andreydantasvf/YourFood/main/.github/view.png',
+    details:
+      'Ecommerce de um restaurante com pratos deliciosos, onde pode-se ver com detalhes a descrição dos pratos e seus ingredientes.',
+    githubLink: 'https://github.com/andreydantasvf/YourFood',
+    webLink: 'https://yourfoodd.netlify.app/'
+  },
+  {
+    title: 'SportFit',
+    imgURL:
+      'https://cdn.discordapp.com/attachments/282658921712320513/1153779306816864257/image.png',
+    details:
+      'Landing page para uma academia ficticia. Construído com Next Js e Tailwind Css',
+    githubLink: 'https://github.com/andreydantasvf/SportFit',
+    webLink: 'https://sportfit-three.vercel.app/'
+  }
+];
