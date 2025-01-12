eef7791a49b6afdc54e973823d2db70f48d42252
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-11 21:54:07 -0300

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import Image from 'next/image';
+import { NavLink } from '../NavLink';
+import { useEffect, useState } from 'react';
+import { Menu, X } from 'lucide-react';
+
+export const Header = () => {
+  const [scrollActive, setScrollActive] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollActive(window.scrollY > 20);
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Bloquear/desbloquear o scroll ao abrir/fechar o menu
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [menuOpen]);
+
+  return (
+    <header
+      className={
+        'w-full pt-4 fixed top-0 z-30 transition-all' +
+        (scrollActive ? ' shadow-md pt-0' : ' pt-4')
+      }
+    >
+      <nav className="max-w-screen-xl py-4 px-6 sm:px-8 lg:px-16 mx-auto flex items-center justify-between">
+        <div>
+          <Image src="/logo.png" alt="Logo" width={64} height={64} />
+        </div>
+
+        {/* Menu para Desktop */}
+        <ul className="hidden md:flex items-center gap-6">
+          <NavLink name="Home" href="#" iconUrl="/zeus.png" />
+          <NavLink name="Sobre Mim" href="#" iconUrl="/poseidon.png" />
+          <NavLink name="Projetos" href="#" iconUrl="/kronos.png" />
+          <NavLink name="Tecnologias" href="#" iconUrl="/hephaestus.png" />
+          <NavLink name="Contatos" href="#" iconUrl="/hades.png" />
+        </ul>
+
+        {/* Botão Menu Hamburguer */}
+        <div
+          className="flex md:hidden items-center cursor-pointer transition-all duration-300 ease-in-out text-primaryColor"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {!menuOpen ? <Menu size={32} /> : <X size={32} />}
+        </div>
+
+        {/* Menu Mobile */}
+        <div
+          className={`md:hidden absolute top-full left-0 w-full bg-background flex flex-col items-center justify-center shadow-md transition-all duration-500 ease-in-out ${
+            menuOpen
+              ? 'opacity-100 translate-y-0'
+              : 'opacity-0 -translate-y-full pointer-events-none'
+          }`}
+          style={{ height: 'calc(100vh - 64px)' }}
+        >
+          <ul className="flex flex-col h-full gap-4 py-4 w-fit mx-auto">
+            <NavLink name="Home" href="#" iconUrl="/zeus.png" />
+            <NavLink name="Sobre Mim" href="#" iconUrl="/poseidon.png" />
+            <NavLink name="Projetos" href="#" iconUrl="/kronos.png" />
+            <NavLink name="Tecnologias" href="#" iconUrl="/hephaestus.png" />
+            <NavLink name="Contatos" href="#" iconUrl="/hades.png" />
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
