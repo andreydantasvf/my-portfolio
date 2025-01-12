f39e5d795d87a534a25720c69e7fd5a5ee6f2144
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-11 23:42:28 -0300

    feat: add news fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ export default {
         foreground: 'var(--foreground)',
         primaryColor: 'var(--primary-color)',
         secondaryColor: 'var(--secondary-color)'
+      },
+      fontFamily: {
+        CormorantGaramond: ['var(--font-cormorant-garamond)', 'serif'],
+        Cinzel: ['var(--font-cinzel)', 'serif']
+      },
+      animation: {
+        animate: 'animate 8s linear infinite'
+      },
+      keyframes: {
+        animate: {
+          '0%, 10%, 100%': { width: '0' },
+          '70%, 90%': { width: '100%' }
+        }
+      },
+      dropShadow: {
+        gold: '0 0 20px #dab083'
       }
     }
   },
