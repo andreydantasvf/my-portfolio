e0cba431932b33cbbce3ac026486c7b4447a4b67
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-12 21:14:31 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { AboutMe } from '@/components/AboutMe';
 import { ContactMe } from '@/components/ContactMe';
+import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import { HomeSection } from '@/components/HomeSection';
 import { Projects } from '@/components/Projects';
@@ -9,13 +10,14 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="h-screen 2xl:h-[620px] 2xl:mt-28 max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto">
+      <main className="2xl:mt-28 max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto">
         <HomeSection />
         <AboutMe />
         <Skills />
         <Projects />
         <ContactMe />
       </main>
+      <Footer />
     </>
   );
 }
