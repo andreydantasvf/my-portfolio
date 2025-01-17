ab02207087887238fdde46c73cfa786297f157cb
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-17 11:18:22 -0300

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,6 +107,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
       </Head>
       <body
         className={`${CormorantGaramond.variable} ${CinzelFont.variable} ${PoppinsFont.variable} antialiased`}
